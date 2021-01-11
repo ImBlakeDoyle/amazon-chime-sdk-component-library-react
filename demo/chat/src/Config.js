7f5a3963a99a76dcf01d2c64ea6ba9cd1eb5f763
Blake Doyle <blake.doyle@genesiscare.com>
2021-01-11 14:30:59 +1100

    initial commit

--- demo/chat/src/Config.js
+++ demo/chat/src/Config.js
@@ -2,11 +2,13 @@
 // SPDX-License-Identifier: Apache-2.0
 
 const appConfig = {
-  cognitoUserPoolId: '',
-  cognitoAppClientId: '',
-  cognitoIdentityPoolId: '',
-  appInstanceArn: '',
+  cognitoUserPoolId: 'us-east-1_bhnaXYP1U',
+  cognitoAppClientId: '4lid5mmr9n2q1pksr7h7o9g4e1',
+  cognitoIdentityPoolId: 'us-east-1:1b4aea45-f5c1-4e3e-98a0-c2ae0d9f73c5',
+  appInstanceArn:
+    'arn:aws:chime:us-east-1:368167353552:app-instance/9002b10e-396d-46e6-b470-69442e9f4e0b',
   region: 'us-east-1',
-  attachments_s3_bucket_name: ''
+  attachments_s3_bucket_name:
+    'gcpatientmatch-chatattachmentsbucket-zf5ils0zddni',
 };
 export default appConfig;
